c51c6f7632e2e8ece05efc7474b5c2dd27a534a9
Juan Diego <jdramirezz@uqvirtual.edu.co>
2019-10-16 09:33:33 -0500

    Creación del repositorio

---
+++ main.py
@@ -0,0 +1,82 @@
+
+
+clients = 'Juan,Henao,'
+
+
+def create_client(client_name):
+    global clients
+
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        warning()
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.strip(','+client_name)
+    else:
+        warning()
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',',updated_client_name + ',')
+    else:
+        warning()
+
+
+def warning():
+    print('Client is no in client\'s list')
+
+def _add_comma():
+    global clients
+
+    clients +=','
+
+
+def _print_welcome():
+    print('Welcome to Juan Ventas')
+    print('*'*50)
+    print('What would you like to do today?')
+    print('[U]pdate client')
+    print('[C]reate client')
+    print('[D]elete client')
+
+def _get_client_name():
+    return input('What is the client name?')
+
+
+if __name__=='__main__':
+    _print_welcome()
+
+    command = input()
+    command = command.upper()
+
+    if command == 'C':
+        client_name = _get_client_name()
+        create_client(client_name)
+        list_clients()
+
+    elif command =='D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name?')
+        update_client(client_name, updated_client_name)
+        list_clients()
+    else:
+        print('Invalid command')
