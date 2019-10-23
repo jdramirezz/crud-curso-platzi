ecb9cb067daa19c7752e86ecc9b348552182b0c0
Juan Diego <jdramirezz@uqvirtual.edu.co>
2019-10-22 20:07:50 -0500

    Correción funciones, creación csv y documento de decoradores

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-import sys
+import csv
+import os
 
-clients = [
-    {
-        'name': 'Juan',
-        'company': 'Google',
-        'email': 'jdramirezz@uqvirtual.edu.co',
-        'position': 'software engineer',
-    },
-    {
-        'name': 'Henao',
-        'company': 'Bexter',
-        'email': 'jhenaohenao@gmail.com',
-        'position': 'Software engineer and designer',
-    }
-]
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+clients = []
 
 
-
-def create_client():
+def create_client(client):
     global clients
 
     if client not in clients:
         clients.append(client)
     else:
-        warning()
+        print('Client is already in client\s list')
 
 
 def list_clients():
+    print('uid |  name  | company  | email  | position ')
+    print('*' * 50)
+
     for idx, client in enumerate(clients):
         print('{uid} | {name} | {company} | {email} | {position}'.format(
         uid = idx,
@@ -36,38 +28,71 @@ def list_clients():
         position = client['position']))
 
 
-def search_client(client_name):
+def update_client(client_id, updated_client):
     global clients
 
-    for client in clients:
-        if client != client_name:
-            continue
-        else:
-            return True
+    if len(clients) - 1 >= client_id:
+        clients[client_id] = updated_client
+    else:
+        print('Client not in client\'s list')
 
 
 def delete_client(client_name):
     global clients
 
-    for client in clients:
-        if client_name == client.get('name'):
-            clients.remove(client)
+    for idx, client in enumerate(clients):
+        if idx == client_id:
+            del clients[idx]
             break
     else:
         warning()
 
 
-def update_client(client_name, updated_client_name):
-    global clients
-
+def search_client(client_name):
     for client in clients:
-        if client_name == client.get('name'):
-            clients.remove(client)
-            new_client = updated_client_name
-            clients.append(new_client)
+        if client['name'] != client_name:
+            continue
+        else:
+            return True
 
-    else:
-        warning()
+
+
+def _get_client_field(field_name, message='What is the client {}?'):
+    field = None
+
+    while not field:
+        field = input(message.format(field_name))
+
+    return field
+
+
+def _get_client_from_user():
+    client = {
+        'name': _get_client_field('name'),
+        'company': _get_client_field('company'),
+        'email': _get_client_field('email'),
+        'position': _get_client_field('position'),
+    }
+
+    return client
+
+
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+        for row in reader:
+            clients.append(row)
+
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+        writer.writerows(clients)
+
+        os.remove(CLIENT_TABLE)
+        os.rename(tmp_table_name, CLIENT_TABLE)
 
 
 def warning():
@@ -85,67 +110,31 @@ def _print_welcome():
     print('[S]earch client')
 
 
-def _get_client_field(field_name):
-    field = None
-
-    while not field:
-        field = input('What is the client {}?'.format(field_name))
-
-    return field
-
-
-
-def _get_client_name():
-    client_name = None
-
-    while not client_name:
-        client_name = input('What is the client name?')
-
-        if client_name == 'exit':
-            client_name = None
-            break
-
-    if not client_name:
-            sys.exit()
-
-    return client_name
-
 
 if __name__=='__main__':
+    _initialize_clients_from_storage()
     _print_welcome()
 
     command = input()
     command = command.upper()
 
     if command == 'C':
-        client = {
-         'name': _get_client_field('name'),
-         'company': _get_client_field('company'),
-         'email': _get_client_field('email'),
-         'position': _get_client_field('position'),
-        }
-        create_client()
-        list_clients()
+        client = _get_client_from_user()
 
-    elif command =='D':
-        client_name = _get_client_name()
-        delete_client(client_name)
-        list_clients()
-
-    elif command == 'U':
-        client_name = _get_client_name()
-        updated_client_name = {
-         'name': _get_client_field('name'),
-         'company': _get_client_field('company'),
-         'email': _get_client_field('email'),
-         'position': _get_client_field('position'),
-        }
-        update_client(client_name, updated_client_name)
-        list_clients()
+        create_client(client)
     elif command == 'L':
         list_clients()
+    elif command == 'U':
+        client_id = int(_get_client_field('id'))
+        updated_client = _get_client_from_user()
+
+        update_client(client_id, updated_client)
+    elif command =='D':
+        client_id = int(_get_client_field('id'))
+
+        delete_client(client_id)
     elif command == 'S':
-        client_name = _get_client_name()
+        client_name = _get_client_field('name')
         found = search_client(client_name)
 
         if found:
@@ -154,3 +143,6 @@ if __name__=='__main__':
             print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command')
+
+
+    _save_clients_to_storage()
